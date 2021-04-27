98b183a27325c1b7dd966885e1f42a2dcf860a95
gvc14 <gvc14041996@gmail.com>
2021-04-27 17:23:29 +0530

    some changes

--- routes/callback.js
+++ routes/callback.js
@@ -12,7 +12,7 @@ var dbclient = new MongoClient(dbURL,{useUnifiedTopology: true, useNewUrlParser:
 router.get('/', async function(req, res, next) {
     var code = url.parse(req.url,true).query.code;
     await functions.getTokensFromSpotify(spotifyApi,code);
-    res.redirect('https://gauravchaudhari.azurewebsites.net/')
+    res.redirect('https://gauravchaudhari.herokuapp.com/')
 });
 
 module.exports = router;
